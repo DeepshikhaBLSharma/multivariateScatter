ef12b58c9a8e0f3dcf3bd0da0fe505975a162b18
DeepshikhaBLSharma <50496677+DeepshikhaBLSharma@users.noreply.github.com>
2019-08-05 11:38:59 +0530

    Create multivariateScatter.R

---
+++ R/multivariateScatter.R
@@ -0,0 +1,14 @@
+
+multivariate <- function(data,num1,num2,cat)
+{
+  plot(data[,num1],data[,num2],
+       xlim=c(0,400),
+       ylim=c(0,400),
+       xlab=num1,
+       ylab=num2,
+       main  = paste('Scatter plot of',num1,'and',num2,'by',cat),
+       col= ifelse(data[,cat]=="True","red","blue"))
+  legend("topright",c("True","False"),
+         col=c("red","blue"),
+         pch=1)
+}
